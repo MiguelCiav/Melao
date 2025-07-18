ad6e51d31209be811f0a56749441f6adf60a4d6e
MiguelCiav <miguelciavato@gmail.com>
2025-07-18 19:20:45 -0400

    Archivos javascript incrustados en los template

--- MelaoApp/urls.py
+++ MelaoApp/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('chat/', views.chat_view, name='chat_view'),
     path('friends/', views.friends_list, name='friends_list'),
     path('config/language-theme/', views.language_and_theme_config_view, name='language_and_theme_config_view'),
-    path('login/', views.login_view, name='login_view'),
     path('profile/edit/', views.modify_profile, name='modify_profile'),
     path('new-post/', views.new_post_view, name='new_post_view'),
     path('post/', views.post_view, name='post_view'),
