1a9bb9ccd3567554e6328467c2a6e7d1e9e882f9
luiferdc <luiferroda@gmail.com>
2025-08-06 21:07:17 -0400

    Correccion minima del settings.py para usar el runserver de manage.py

--- Melao/settings.py
+++ Melao/settings.py
@@ -84,10 +84,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/sd')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+#]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
